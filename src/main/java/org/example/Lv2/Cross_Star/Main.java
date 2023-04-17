2fa25a1f99d7f18dc22efbadbf5cf09b18324bf8
Y39 <y39scode@gmail.com>
2023-04-17 19:58:30 +0900

    Programmers: lv2.CrossStar 87377

--- src/main/java/org/example/Lv2/Cross_Star/Main.java
+++ src/main/java/org/example/Lv2/Cross_Star/Main.java
@@ -1,17 +1,94 @@
 package org.example.Lv2.Cross_Star;
 
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Integer[][] citations = {{1, -1, 0}, {2, -1, 0}};
+        int[][] coordinate = {{1, -1, 0}, {2, -1, 0}};
         Solution s = new Solution();
-        System.out.println("----------s:"+s.solution(citations));
+        System.out.println("----------s:"+s.solution(coordinate));
 
     }
 }
 class Solution {
-    public String[] solution(Integer[][] citations) {
-
-        return null;
+    Long[] max=new Long[2];
+    Long[] min=new Long[2];
+    Map<Long, Set> integerPointersMap = new HashMap();
+    public String[] solution(int[][] coordinate) {
+        setIntegerPointersMap(coordinate);
+        return draw();
     }
+    public void setIntegerPointersMap(int[][] coordinate){
+        for(int i=0; i<coordinate.length-1; i++ ){
+            for(int n=i+1; n<coordinate.length; n++){
+                Double[] pointer = calcIntersectionPoint(coordinate[i], coordinate[n]);
+//                System.out.println(">>>>>>>>>>>>>교점:"+ Arrays.toString(pointer)+isInteger(pointer) );
+                if(isInteger(pointer)){
+                    long x = Math.round(pointer[0]);
+                    long y = Math.round(pointer[1]);
+                    if(integerPointersMap.containsKey(y))
+                        integerPointersMap.get(y).add(x);
+                    else
+                        integerPointersMap.put(y, new HashSet(){{
+                            add(x);
+                        }});
+                    comparePointerTo(pointer);
+                }
+//                System.out.println(String.format("Max:%s//Min:%s",Arrays.toString(max), Arrays.toString(min)));
+            }
+        }
+    }
+    private void comparePointerTo(Double[] pointer) {
+        for(int i=0; i<pointer.length; i++){
+            if(min[i] == null)
+                min[i] = Math.round(pointer[i]) ;
+            if(max[i] == null)
+                max[i] = Math.round(pointer[i]);
+            if(pointer[i]<min[i])
+                min[i]=Math.round(pointer[i]);
+            if(pointer[i]>max[i])
+                max[i]=Math.round(pointer[i]);
+        }
+    }
+
+    public Double[] calcIntersectionPoint(int[] formula_1, int[] formula_2){
+        double A = formula_1[0];
+        double B = formula_1[1];
+        double C = formula_2[0];
+        double D = formula_2[1];
+        double E = formula_1[2];
+        double F = formula_2[2];
+        //System.out.println(String.format("A=%f/B=%f/C=%f/D=%f/E=%f/F=%f", A,B,C,D,E,F));
+        double divisor = A*D - B*C;
+        if(divisor==0)
+            return new Double[]{0.1, 0.1};
+        return new Double[]{(B*F - E*D) / divisor, (E*C - A*F) / divisor};
+    }
+
+    public boolean isInteger(Double[] pointer){
+        return pointer[0]%1 == 0 && pointer[1]%1==0;
+    }
+
+    public String[] draw(){
+        int colSize = (int)(max[1] - min[1] + 1);
+        int rowSize = (int)(max[0] - min[0] + 1);
+        String[] result = new String[(int)colSize];
+
+        for(int i=0; i<colSize; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int n=0; n<rowSize; n++){
+                if(integerPointersMap.containsKey(i+min[1])
+                 && integerPointersMap.get(i+min[1]).contains(n+min[0]))
+                   sb.append("*");
+                else
+                    sb.append(".");
+
+            }
+            result[colSize-i-1]=sb.toString();
+        }
+        return result;
+    }
+
 }
+
--- src/test/java/org/example/Lv2/Cross_Star/SolutionTest.java
+++ src/test/java/org/example/Lv2/Cross_Star/SolutionTest.java
@@ -1,14 +1,51 @@
 package org.example.Lv2.Cross_Star;
 
+import org.assertj.core.api.Assertions;
+import org.example.more.StudyGroup.week2.q1_SqrtSort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+    int[][] coordinate;
     @Test
-    @DisplayName("[[1, -1, 0], [2, -1, 0]] => [*]")
+    @DisplayName("[[1, -1, 0], [2, -1, 0]]의 교점은 (0, 0)")
     void t1(){
+        coordinate = new int[][]{{1, -1, 0}, {2, -1, 0}};
+        Assertions.assertThat(new Solution().calcIntersectionPoint(coordinate[0], coordinate[1])).isEqualTo(new double[]{0, 0});
+    }
+
+    @Test
+    @DisplayName("[0, 1, -1], [1, 0, -1], [1, 0, 1] => [*.*])")
+    void t2(){
+        coordinate = new int[][]{{0, 1, -1}, {1, 0, -1}, {1, 0, 1}};
+        Assertions.assertThat(new Solution().solution(coordinate)).isEqualTo(new String[] {"*.*"});
+    }
+
+    @Test
+    @DisplayName("[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12] , [5, 8, 12]" +
+            "=> [\"....*....\", " +
+            "    \".........\", " +
+            "    \".........\", " +
+            "    \"*.......*\", " +
+            "    \".........\", " +
+            "    \".........\", " +
+            "    \".........\", " +
+            "    \".........\", " +
+            "    \"*.......*\"]")
+    void t3(){
+        coordinate = new int[][]{{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+        Assertions.assertThat(new Solution().solution(coordinate)).isEqualTo(new String[] {
+                "....*....",
+                ".........",
+                ".........",
+                "*.......*",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                "*.......*"});
 
     }
 
