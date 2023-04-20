cf08409d6176ded33705353d4d48ffaf080b2996
Y39 <y39scode@gmail.com>
2023-04-20 11:21:04 +0900

    자료구조: put, remove, get, size 메소드 구현

--- src/main/java/org/example/more/myHashMap/MyHashMap.java
+++ src/main/java/org/example/more/myHashMap/MyHashMap.java
@@ -1,18 +1,73 @@
 package org.example.more.myHashMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyHashMap <T,F>{
     int size;
+    List[] hashArr;
 
     public MyHashMap() {
+        size=0;
+        hashArr = new List[12];
+        for(int i=0; i<12; i++){
+            hashArr[i]=new ArrayList<Object[]>();
+        }
     }
 
 
     public F put(T key, F value) {
-        System.out.println(value.hashCode());
-        return value;
+        Object[] currentNode = getNodeAndIndex(key);
+        F preValue;
+        int hashKey = key.hashCode()%12;
+        if(currentNode !=null){
+            preValue = (F)currentNode[1];
+            currentNode[1] = value;
+        }
+        else{
+            ++size;
+            preValue = null;
+            hashArr[hashKey].add(new Object[]{key, value});
+        }
+        return preValue;
     }
+
+    public F get(T key){
+        Object[] node = getNodeAndIndex(key);
+        return node==null? null : (F)node[1];
+    }
+    private Object[] getNodeAndIndex(T key){
+        List<Object[]> nodes = getSameHashcodeNodes(key);
+        if(nodes.size()!=0){
+            for(int i=0; i<nodes.size(); i++){
+                Object[] node = nodes.get(i);
+                if(((T)node[0]).equals((T)key))
+                    return new Object[]{node[0], node[1], i};
+            }
+        }
+        return null;
+    }
+
 
     public int size(){
         return size;
+    }
+
+    private List<Object[]> getSameHashcodeNodes(T key){
+        return hashArr[key.hashCode()%12];
+    }
+
+    public F remove(T key) {
+        List<Object[]> nodes = getSameHashcodeNodes(key);
+        Object[] nodeAndIdx = getNodeAndIndex(key);
+        if(nodeAndIdx == null)
+            return null;
+        nodes.remove((int) nodeAndIdx[2]);
+        --size;
+        return (F)nodeAndIdx[1];
+    }
+
+    public boolean containsKey(T key) {
+        return getNodeAndIndex(key) != null;
     }
 }
--- src/test/java/org/example/more/myHashMap/MyHashMapTest.java
+++ src/test/java/org/example/more/myHashMap/MyHashMapTest.java
@@ -15,8 +15,48 @@ class MyHashMapTest {
     @Test
     void testPut() {
         assertNull(map.put("Key1", 1));
+        assertNull(map.put("Key2", 1));
+        assertNull(map.put("Key3", 1));
+        assertNull(map.put("apple", 1));
+        assertEquals(4, map.size());
+    }
+
+    @Test
+    void testPutReplace() {
+        map.put("Key1", 1);
+        assertEquals(Integer.valueOf(1), map.put("Key1", 2));
+        assertEquals(1, map.size());
+
+    }
+    @Test
+    void testGet() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertEquals(Integer.valueOf(1), map.get("Key1"));
+        assertEquals(Integer.valueOf(2), map.get("Key2"));
+    }
+    @Test
+    void testRemove() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertEquals(Integer.valueOf(1), map.remove("Key1"));
+        assertNull(map.remove("Key3"));
         assertEquals(1, map.size());
     }
 
+    @Test
+    void testSize() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        map.put("Key3", 3);
+        assertEquals(3, map.size());
+    }
+    @Test
+    void testContainsKey() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertTrue(map.containsKey("Key1"));
+        assertFalse(map.containsKey("Key3"));
+    }
 
 }
